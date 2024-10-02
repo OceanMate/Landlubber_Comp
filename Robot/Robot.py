2d020baf9efb986dde71e092ca96fe68eb2a4549
Sawyer <sawyer.c.theis@gmail.com>
2024-10-02 10:41:07 -0500

    add initial code structure

---
+++ Robot/Robot.py
@@ -0,0 +1,20 @@
+class Robot:
+    def robot_init():
+        
+        return
+    
+    def robot_perodic():
+        
+        return
+    
+    def teleop_init():
+        
+        return
+    
+    def teleop_periodic():
+            
+        return
+    
+    def disabled_init():
+        
+        return
---
+++ structure/Command.py
@@ -0,0 +1,21 @@
+class Command:
+    # Called when the command is first run
+    # should be overridden by the child class
+    def init(self):
+        pass
+    
+    # Called every time while the command isn't finished
+    # should be overridden by the child class
+    def execute(self):
+        pass
+    
+    # Called when the command is finished or interrupted
+    # interrupted is a boolean that is true if the command was interrupted
+    # should be overridden by the child class
+    def end(self, interrupted):
+        pass
+    
+    # Called to check if the command is finished
+    # should be overridden by the child class
+    def is_finished(self):
+        return True
