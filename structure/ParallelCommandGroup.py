b8cf63637cc523505dff113d9d628895e801cb26
Sawyer <sawyer.c.theis@gmail.com>
2024-10-09 16:54:02 -0500

    Added Parallel and sequential command groups

---
+++ structure/ParallelCommandGroup.py
@@ -0,0 +1,48 @@
+from structure.Command import Command
+
+class SequentialCommandGroup(Command):
+    def __init__(self):
+        super().__init__()
+        self.commands = []
+    
+    def add_commands(self, *commands):
+        for command in commands:
+            for requirement in command.get_requirements():
+                # add each commands requirements if not already in the list
+                if requirement not in self.requirements:
+                    self.requirements.append(requirement)
+            # add the command to the list of commands to be excuted in order
+            self.commands.append(command)
+    
+    def initalize(self):        
+        # initalize the all commands
+        for command in self.commands:
+            command.initalize()
+        
+    
+    def execute(self):
+        # if there are no commands to execute, return
+        if len(self.commands) <= 0:
+            return
+        
+        for command in self.commands:
+            command.execute();
+            
+            if command.is_finished():
+                command.end(False);
+
+    
+    
+    def end(self, interrupted):
+        # if the command is interrupted and there are still commands to be executed, end every command
+        if interrupted and len(self.commands) != 0:
+            for command in self.commands:
+                command.end(True)
+    
+        # remove all commands
+        for command in self.commands:
+            self.commands.remove(command)
+    
+    # returns true if all commands have been finished
+    def is_finished(self):
+        return len(self.commands) <= 0
---
+++ structure/SequentialCommandGroup.py
@@ -0,0 +1,55 @@
+from structure.Command import Command
+
+class SequentialCommandGroup(Command):
+    def __init__(self):
+        super().__init__()
+        self.commands = []
+        self.current_command = -1
+    
+    def add_commands(self, *commands):
+        for command in commands:
+            for requirement in command.get_requirements():
+                # add each commands requirements if not already in the list
+                if requirement not in self.requirements:
+                    self.requirements.append(requirement)
+            # add the command to the list of commands to be excuted in order
+            self.commands.append(command)
+    
+    def initalize(self):
+        self.current_command = 0
+        
+        # initalize the first command
+        if len(self.commands) > 0:
+            self.commands[self.current_command].initalize()
+        
+    
+    def execute(self):
+        # if there are no commands to execute, return
+        if len(self.commands) <= 0:
+            return
+        
+        currentCommand = self.commands[self.current_command]
+
+        currentCommand.execute();
+        
+        # if the current command is finished, end it and move to the next command
+        if currentCommand.is_finished():
+            currentCommand.end(False);
+            self.current_command += 1
+            if self.current_command < len(self.commands):
+                self.commands[self.current_command].initalize()
+            
+        
+    
+    def end(self, interrupted):
+        # if the command is interrupted and there are still commands to be executed, end the current command
+        if interrupted and len(self.commands) != 0:
+            if self.current_command > -1 and self.current_command < len(self.commands):
+                self.commands[self.current_command].end(True)
+    
+        # stop all commands
+        self.current_command = -1;
+    
+    # returns true if all commands have been finished
+    def is_finished(self):
+        return self.current_command >= len(self.commands)
