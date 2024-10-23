fc82638c9e6f605aee04bc4550e1eba55a009a66
Sawyer <sawyer.c.theis@gmail.com>
2024-10-22 19:42:46 -0500

    rename some vars to make it fit code

--- Robot/Subsystems/LinearMotors.py
+++ Robot/Subsystems/LinearMotors.py
@@ -6,26 +6,25 @@ class LinearMotors(Subsystem):
     
     # Runs the motors given the x, y, and z speeds (each from -1 to 1)
     def runMotors(self, xSpeed, ySpeed, zRotation):
-        # Tennille code goes here
         # calculate the absolute values
-        abs_x = abs(x)
-        abs_y = abs(y)
-        abs_z = abs(z)
+        abs_x = abs(xSpeed)
+        abs_y = abs(ySpeed)
+        abs_z = abs(zRotation)
 
         # Calculate the sum of absolute values
         total_sum = abs_x + abs_y + abs_z
 
         # actual matrix input
-        X = x / total_sum
-        Y = y / total_sum
-        Z = z / total_sum
+        reducedX = xSpeed / total_sum
+        reducedY = ySpeed / total_sum
+        reducedZ = zRotation / total_sum
 
         # motor matrix output
-        FL = X + Y + Z
-        FR = X - Y - Z
-        BR = Z - X - Y
-        BL = Y - X - Z
-        pass
+        FL = reducedX + reducedY + reducedZ
+        FR = reducedX - reducedY - reducedZ
+        BR = reducedZ - reducedX - reducedY
+        BL = reducedY - reducedX - reducedZ
+        
     
     def __setMotor(self, motor, speed):
         # TODO define better
