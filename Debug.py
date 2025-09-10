ea1bb1c84936e6a94c84e95e2e0c97f86113f03f
KableMorrill <kable.morrill@gmail.com>
2025-09-09 19:01:11 -0500

    Commit test

--- Debug.py
+++ Debug.py
@@ -1,6 +1,6 @@
 class Debug:
     # Setting this to True will disable all communication with the Nautical comp
-    disableComs = False
+    disableComs = True
     # Setting this to True will allow for enabling the robot without a connection to the Nautical comp
     ignoreComsToEnable = False
     # Setting this to True will display the FPS of the camera feed on the camera widget
